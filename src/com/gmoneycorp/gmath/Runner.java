b9d6da7cad1abf23e26224ff9cc0bc42ca3bb136
Graham C. Preston <gcpreston@ebsco.com>
2016-08-15 09:29:22 -0400

    Minor bug fix

--- src/com/gmoneycorp/gmath/Runner.java
+++ src/com/gmoneycorp/gmath/Runner.java
@@ -2,35 +2,35 @@ package com.gmoneycorp.gmath;
 
 public class Runner {
 
+	private static String operation;
+	
 	public static void main(String[] args) {
 
 		try {
-			String operation = args[0].toLowerCase();
+			String op = args[0].toLowerCase();
+			operation = op;
 
 			switch (operation) {
 			case "factor":
 				if (args.length > 2) {
-					returnUsage(operation);
+					returnUsage();
 					break;
 				}
 				System.out.println("Factors of " + args[1] + ": " + GMath.factor(parseInt(args[1])));
 				break;
 			case "gcf":
 				if (args.length > 3) {
-					returnUsage(operation);
+					returnUsage();
 					break;
 				}
 				System.out.println("GCF of " + args[1] + " and " + args[2] + ": " + GMath.findGCF(parseInt(args[1]), parseInt(args[2])));
 				break;
 			default:
-				returnUsage("null");
+				returnUsage();
 				break;
 			}
 		} catch (ArrayIndexOutOfBoundsException e) {
-			if (args.length > 0)
-				returnUsage(args[0].toLowerCase());
-			else
-				returnUsage("null");
+			returnUsage();
 			System.exit(0);
 		}
 	}
@@ -38,20 +38,27 @@ public class Runner {
 	public static int parseInt(String s) {
 		try {
 			if (s == null) {
-				returnUsage("null");
+				returnUsage();
 				System.exit(0);
 				return 0;
 			} else {
 				return Integer.parseInt(s);
 			}
 		} catch (final NumberFormatException ex) {
-			returnUsage(null);
+			returnUsage();
 			System.exit(0);
 			return 0;
 		}
 	}
 	
-	public static void returnUsage(String operation) {
+	public static void returnUsage() {
+		String usage = "Usage: java -jar gmath.jar [operation] [input]\n\nwhere operations include:\n\tfactor: return a list of factors of an integer\n\tgcf: return the greatest common factor of two integers";
+		
+		if (operation == null) {
+			System.out.println(usage);
+			System.exit(0);
+		}
+		
 		switch (operation) {
 		case "factor":
 			System.out.println("Usage: java -jar gmath.jar factor [input]\nwhere input is an integer.");
@@ -60,8 +67,7 @@ public class Runner {
 			System.out.println("Usage: java -jar gmath.jar gcf [input]\nwhere input is two integers.");
 			break;
 		default:
-			System.out.println("Usage: java -jar gmath.jar [operation] [input]\n\nwhere operations include:\n\tfactor: return a list of factors "
-					+ "of an integer\n\tgcf: return the greatest common factor of two integers");
+			System.out.println(usage);
 		}
 	}
 }
